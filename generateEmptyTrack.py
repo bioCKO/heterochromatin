afaefb275f433ca7e10e3964f08099bf6e9c6d4f
biomonika NA <biomonika@psu.edu>
2016-11-04 16:17:55 +0000

    generateEmptyTrack.py created online with Bitbucket

---
+++ generateEmptyTrack.py
@@ -0,0 +1,65 @@
+import sys
+import os
+from os.path import basename
+import subprocess as sp
+import itertools
+from itertools import izip
+
+######################################
+#python extract_IPDs_to_windows.py path_to_tract path_to_pickle_file
+motifFile=str(sys.argv[1])
+######################################
+
+print motifFile
+num_lines = open(motifFile).read().count('\n')
+
+print num_lines
+outputEmpty=motifFile + "EmptyTmp"
+
+#sample same number of empty lines as are in motif file
+bashCommand = "shuf -n " + str(num_lines) + " " + "Empty"
+print bashCommand
+
+#write them to the file with suffix 'EmptyTmp'
+p = sp.Popen(bashCommand.split(), stdin = sp.PIPE, stdout = sp.PIPE, stderr = sp.PIPE)
+
+#read at the same time motifFile and EmptyTmp file and generate an output which will represent appropriate modeling of the empty windows with lengths mirroring lengths in feature files
+#print motifFile
+#print outputEmpty
+
+mf = open(motifFile,'r').readlines()
+ef = p.stdout.read()
+#print ef
+
+eff=ef.split('\n')
+#print eff
+
+f = open(outputEmpty, 'w')
+
+print "looping"
+for i in range(0,num_lines):
+	#print i
+	m=mf[i].rstrip() #line from motif file
+	e=eff[i].rstrip() #line from matched empty file
+
+	m_array=m.split('\t')
+	e_array=e.split('\t')
+	print (m_array[0:3])
+	print (e_array[0:3])
+
+	m_length=int(m_array[2])-int(m_array[1])+1
+	e_middle_point=int(e_array[1])+(int(e_array[2])-int(e_array[1]))/2+1
+	print ("motif length: " + str(m_length))
+	print ("e_middle_point: " + str(e_middle_point))
+	if (m_length % 2 == 0): #even 
+		print "even"
+		res=(e_array[0] + "\t" + str(e_middle_point-(m_length/2)) + "\t" + str(e_middle_point+(m_length/2)-1))
+	else: #odd
+		print "odd"
+		res=(e_array[0] + "\t" + str(e_middle_point-(m_length/2)) + "\t" + str(e_middle_point+(m_length/2)))
+	print res
+	f.write(res+"\n")
+	print "***"
+
+
+
