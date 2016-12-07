420fdbb0e250dbe8e8da28c6fef247db002699fa
biomonika NA <biomonika@psu.edu>
2016-12-07 02:23:02 +0000

    note that empty can be composed from any chromosomes

--- generateEmptyTrack.py
+++ generateEmptyTrack.py
@@ -4,6 +4,7 @@ from os.path import basename
 import subprocess as sp
 import itertools
 from itertools import izip
+import math
 
 ######################################
 #python generateEmptyTrack.py motifFile
@@ -17,7 +18,7 @@ print num_lines
 outputEmpty=motifFile + "EmptyTmp"
 
 #sample same number of empty lines as are in motif file
-bashCommand = "shuf -n " + str(num_lines) + " " + "Empty"
+bashCommand = "shuf -n " + str(num_lines) + " " + "Empty.mf"
 print bashCommand
 
 p = sp.Popen(bashCommand.split(), stdin = sp.PIPE, stdout = sp.PIPE, stderr = sp.PIPE)
@@ -36,22 +37,30 @@ for i in range(0,num_lines):
 
 	m_array=m.split('\t')
 	e_array=e.split('\t')
-	print (m_array[0:3])
-	print (e_array[0:3])
+	#print (e_array[0:3])
 
-	m_length=int(m_array[2])-int(m_array[1])+1
-	e_middle_point=int(e_array[1])+(int(e_array[2])-int(e_array[1]))/2+1
-	print ("motif length: " + str(m_length))
-	print ("e_middle_point: " + str(e_middle_point))
-	if (m_length % 2 == 0): #even 
+	window_start=int(e_array[1]) #coordinates from empty file
+	window_end=int(e_array[2]) #coordinates from empty file
+	length=int(m_array[3]) #length from motif file
+	tail=e_array[4:len(e_array)]
+		#print ("tail: " + str(tail))
+
+		#print ("motif length: " + str(length))
+
+	if (length % 2 == 0): #even 
 		print "even"
-		res=(e_array[0] + "\t" + str(e_middle_point-(m_length/2)) + "\t" + str(e_middle_point+(m_length/2)-1))
+		feature_start = window_start + (50 - math.trunc(length / 2))
+		feature_stop = window_start + (50 + math.trunc(length / 2)-1)
+		IPDsubset = tail[(50 - math.trunc(length / 2)):(50 + math.trunc(length / 2))]
 	else: #odd
 		print "odd"
-		res=(e_array[0] + "\t" + str(e_middle_point-(m_length/2)) + "\t" + str(e_middle_point+(m_length/2)))
-	print res
+		feature_start = window_start + (50 - math.trunc(length / 2))
+		feature_stop = window_start + (50 + math.trunc(length / 2))
+		IPDsubset = tail[(50 - math.trunc(length / 2)):(50 + math.trunc(length / 2))]
+	res=(e_array[0] + "\t" + str(feature_start) + "\t" + str(feature_stop) + "\t" + str(length)+ "\t" + '\t'.join(IPDsubset))
+	#print res
 	f.write(res+"\n")
-	print "***"
+	#print "***"
 
 
 
