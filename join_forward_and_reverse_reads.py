c76dc76a6265265c0d0f4feaeae80dae96829841
biomonika NA <biomonika@psu.edu>
2017-03-07 16:42:13 +0000

    join_forward_and_reverse_reads.py edited online with Bitbucket

--- join_forward_and_reverse_reads.py
+++ join_forward_and_reverse_reads.py
@@ -12,15 +12,23 @@ from itertools import izip
 forward=sys.argv[1]
 reverse=sys.argv[1]
 
-with open(forward) as f:
-	with open(reverse) as r:
-    print(f.readline())
-    print(r.readline())
-    headerF=f.readline()
-    print("headerF: " + headerF)
-    	while True:
-    		lineR=r.readline()
-    		print("lineR: " lineR)
-    			if (headerF==lineR):
-    				print("match")
-        			break
+fRepeats=[]
+rRepeats=[]
+
+f = open(forward)
+r = open(reverse)
+print(f.readline())
+print(r.readline())
+headerF=f.readline()
+print("headerF: " + headerF)
+while True:
+    lineR=r.readline()
+    print("lineR: " + lineR)
+    if (headerF==lineR):
+	    last_pos = r.tell()
+	    while ("@" not in lineR):
+	    	lineR=r.readline()
+	    	print lineR
+	    print("match")
+	    break
+r.seek(last_pos)
