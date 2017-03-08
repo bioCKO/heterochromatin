5ea33b8d9af6c0e16c6db3b0c1e4d814144061da
biomonika NA <biomonika@psu.edu>
2017-03-08 15:58:54 +0000

    join_forward_and_reverse_reads.py edited online with Bitbucket

--- join_forward_and_reverse_reads.py
+++ join_forward_and_reverse_reads.py
@@ -2,8 +2,7 @@ import sys
 import os
 from os.path import basename
 import numpy as np
-import itertools
-from itertools import izip
+import collections
 
 ######################################
 #python join_forward_and_reverse_reads.py forwardReads reverseReads
@@ -12,53 +11,25 @@ from itertools import izip
 forward=sys.argv[1]
 reverse=sys.argv[2]
 
-fRepeats=[]
-rRepeats=[]
-
 f = open(forward)
 r = open(reverse)
-print(f.readline())
-print(r.readline())
 
-#print("lineR: " + lineR)
-headerF=f.readline().rstrip()
-lineR=r.readline().rstrip()
-isHeader=True
-while True:
-    Fnumber=headerF.split(".")[1]
-    print("headerF: " + headerF + " " + Fnumber)
-    if ("@" in headerF):
-        #if (headerF==lineR):
-        #print(isHeader)
-        last_pos = r.tell()
-        while (isHeader==False):
-            lineR=r.readline().rstrip()
-            isHeader=("@" in lineR)
-            #print("isHeader: " + str(isHeader) + " lineR: " + str(lineR))
-            #Rnumber=lineR.split(".")[1]
-            #print("lineR: " + lineR + " " + Rnumber)
-        #r.seek(last_pos)
-        print("checking for match" + headerF + " and " + lineR)
-        isHeader=False #now we need to iterate again through reverse reads
-        #print("headerF: " + headerF + " " + Fnumber)
-        Fnumber=int(headerF.split(".")[1])
-        Rnumber=int(lineR.split(".")[1])
-        if (headerF==lineR):
-            print "Match, read next forward header."
-            isFHeader=False
-            while (isFHeader==False):
-                headerF=f.readline().rstrip()
-                isFHeader=("@" in headerF)
-                #print("isFHeader: " + str(isFHeader))
-            print("We found new forward header: " + headerF)
-        if (Rnumber>Fnumber):
-            print("Forward has no repeats, we need to read next forward, while keeping old reverse.")
-            r.seek(last_pos) #keep old reverse
-            isFHeader=False
-            while (isFHeader==False):
-                headerF=f.readline().rstrip()
-                isFHeader=("@" in headerF)
-                #print("isFHeader: " + str(isFHeader))
-            print("We found new forward header: " + headerF)
+fDict=collections.OrderedDict()
+rDict=collections.OrderedDict()
+fList=[]
 
-#r.seek(last_pos)
+with open(forward) as f:
+    i=0
+    for line in f:
+        line=line.rstrip()
+        i=i+1
+        if ("@" in line):
+            header=line
+            if (i>1):
+                #store results to previous header
+                fDict[header]=fList
+                fList=[]
+        else:
+            fList.append(line)
+    fDict[header]=fList #last entry
+print(fDict)
