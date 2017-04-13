b27b8ad66fbc0852cb3c079ec2cb494141335e2c
biomonika NA <biomonika@psu.edu>
2017-04-13 15:54:14 +0000

    parse_passes.py edited online with Bitbucket

--- parse_passes.py
+++ parse_passes.py
@@ -1,24 +1,24 @@
 import sys
 import os
 from os.path import basename
-import subprocess as sp
 import itertools
 from itertools import izip
 import re
 import collections
 from collections import defaultdict
+#for a in xx*; do head -n 3 $a | tail -n 1 | cut -d' ' -f2 | sed 's/"//g'; python parse_passes.py $a; done;
 
 ######################################
 #python generateEmptyTrack.py motifFile
 motifFile=str(sys.argv[1])
 ######################################
 
-print motifFile
+#print motifFile
 passesDict=defaultdict(int)
 
-outputEmpty=motifFile + "_formatted.txt"
-
+#outputEmpty=motifFile + "_formatted.txt"
 #f = open(outputEmpty, 'w') #write results here
+
 def formatWhitespaces(text):
 	text=text.rstrip() #remove newline
 	text=re.sub(' +',' ',text) #remove multiple whitespaces
@@ -30,27 +30,18 @@ with open(motifFile) as f:
     f.readline() #skip header
     f.readline() #skip header
     for coordinates,space,passes,counts in itertools.izip_longest(*[f]*4):
-        print("***")
+        #print("***")
         coordinates=formatWhitespaces(coordinates)
         passes=formatWhitespaces(passes).split(" ")
         counts=formatWhitespaces(counts).split(" ")
-        print(coordinates,space,passes,counts)
+        #print(coordinates,space,passes,counts)
 
         for i in range(0, len(passes), 1):
         	p=passes[i]
         	c=counts[i]
-        	print("p:" + p + " c:" + c)
+        	#print("p:" + p + " c:" + c)
         	passesDict[p]+=int(c)
 
-print passesDict
-#with open(motifFile) as f:
-#    for line in f:
-    	#if line.strip():
-#    		line=line.rstrip() #remove newline
-#    		line=re.sub(' +',' ',line) #remove multiple whitespaces
-#    		line=re.sub('^ ','',line) #remove leading whitespace
-#        	if ("[" not in line): #we are not interested in coordinates
-#        		print line
-#        		#keys=line
-	#f.write(res+"\n")
-	#print "***"
+
+for k in passesDict.keys():
+	print k, passesDict[k]
