28be20bbee9abc5e6632960703d0f754d3f98ad4
biomonika NA <biomonika@psu.edu>
2017-04-17 13:07:49 +0000

    pacbio mpileup code

--- generate_mpileup.sh
+++ generate_mpileup.sh
@@ -7,8 +7,8 @@ export PATH="/nfs/brubeck.bx.psu.edu/scratch5/wilfried/src/bedtools2-master/bin:
 #PACBIO GENERATE MPILEUP
 folder_with_motifs="/nfs/brubeck.bx.psu.edu/scratch6/monika/pac_errors/latest/features10000"
 bam_folder="/nfs/brubeck.bx.psu.edu/scratch6/monika/pac_errors/passes/sam"
-#array=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X); #chromosomes to use
-array=(10); #chromosomes to use
+array=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X); #chromosomes to use
+
 reference="/nfs/brubeck.bx.psu.edu/scratch5/wilfried/kinetics/data/hg19_formated_by_wil.fa"
 
 
@@ -20,7 +20,6 @@ for motif_file in $folder_with_motifs/*.mf; do
 	echo -n $motif " ";
 	awk -v mtf=$motif '{print $1 "\t mf \t" mtf "\t" ($2) "\t" $3 "\t.\t0\t.\tkinetics"}' $motif_file | sort -k1,1 -k4,4n >${motif}.gff
 	gff2bed < ${motif}.gff > ${motif}.bed
-	sed -i 's/^/chr/g' ${motif}.bed #bed files should contain "chr" for the compatibility with the reference
 done; 
 
 #CONVERT .mf FILES into .GFF and .BED
@@ -31,10 +30,10 @@ for X in "${array[@]}"; do
 	for motif_file in *.mf.bed; do 
 		echo $motif_file; 
 		out=`basename $motif_file`; 
-		samtools mpileup ${bam_folder}/chr${X}_P6.cmp.h5.sam.sorted.bam -f $reference -l ${motif_file} -uv -t INFO/DPR >mp/${X}_${out}.mp &
+		samtools mpileup ${bam_folder}/chr${X}_P6.cmp.h5.bam -f $reference -l ${motif_file} -uv -t INFO/DPR >mp/${X}_${out}.mp &
 	done;
 	wait 
-done;
+done; 
 
 echo "Parsing mpileup output (.mp files)"
 echo "====================================="
