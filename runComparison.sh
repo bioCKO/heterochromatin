8fe6977e13bc3272452ab8e511ec2b268622bb62
biomonika NA <biomonika@psu.edu>
2017-04-25 15:50:23 +0000

    runComparison.sh edited online with Bitbucket

--- runComparison.sh
+++ runComparison.sh
@@ -4,15 +4,18 @@ export PATH="/nfs/brubeck.bx.psu.edu/scratch5/wilfried/src/bedtools2-master/bin:
 a=1; #chromosome 1 
 
 #FEATURE
-bedtools coverage -abam ../sam/forward/chr${a}_P6.cmp.h5.bam.forward.bam -b ../mp_forward_10000/GQuadPlusFeatureOnly.mf.bed -counts >${a}.GQuadPlus.forward.depth &
+bedtools coverage -b ../sam/forward/chr${a}_P6.cmp.h5.bam.forward.bam -a ../mp_forward_10000/GQuadPlusFeatureOnly.mf.bed -counts >GQuadPlus.forward.depth &
 
-bedtools coverage -abam ../sam/reverse/chr${a}_P6.cmp.h5.bam.reverse.bam -b ../mp_forward_10000/GQuadPlusFeatureOnly.mf.bed -counts >${a}.GQuadPlus.reverse.depth &
+bedtools coverage -b ../sam/reverse/chr${a}_P6.cmp.h5.bam.reverse.bam -a ../mp_forward_10000/GQuadPlusFeatureOnly.mf.bed -counts >GQuadPlus.reverse.depth &
 
 #CONTROL
-bedtools coverage -abam ../sam/forward/chr${a}_P6.cmp.h5.bam.forward.bam -b ../mp_forward_10000_replicate1/GQuadPlusFeatureOnlyEmptyTmp.mf.bed -counts >${a}.GQuadPlusControl.forward.depth &
+bedtools coverage -b ../sam/forward/chr${a}_P6.cmp.h5.bam.forward.bam -a ../mp_forward_10000_replicate1/GQuadPlusFeatureOnlyEmptyTmp.mf.bed -counts >GQuadPlusControl.forward.depth &
 
-bedtools coverage -abam ../sam/reverse/chr${a}_P6.cmp.h5.bam.reverse.bam -b ../mp_forward_10000_replicate1/GQuadPlusFeatureOnlyEmptyTmp.mf.bed -counts >${a}.GQuadPlusControl.reverse.depth &
+bedtools coverage -b ../sam/reverse/chr${a}_P6.cmp.h5.bam.reverse.bam -a ../mp_forward_10000_replicate1/GQuadPlusFeatureOnlyEmptyTmp.mf.bed -counts >GQuadPlusControl.reverse.depth &
 
 wait
 
+for d in *depth; do
+	grep "^{a}\b" $d >${a}.${d}
+done
 echo "Done."
