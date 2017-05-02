4c0a328cbe5d01dea5abac063bdcfbb6fff81fce
biomonika NA <biomonika@psu.edu>
2017-05-02 17:20:34 +0000

    reorder.py edited online with Bitbucket

--- reorder.py
+++ reorder.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import os.path
 from os.path import basename
 import itertools
 from itertools import izip
@@ -14,11 +15,17 @@ motifFileWithIPds=str(sys.argv[1])
 motifFileWithErrors=str(sys.argv[2])
 ######################################
 
+print("motifFileWithIPds: " + motifFileWithIPds + "; " + "motifFileWithErrors: " + motifFileWithErrors)
+
 #print motifFile
 errorDict=defaultdict(int)
 
-#outputEmpty=motifFile + "_formatted.txt"
-#f = open(outputEmpty, 'w') #write results here
+outputEmpty=motifFileWithErrors.replace("merged_","")
+if (os.path.exists(outputEmpty)):
+	print("File " + outputEmpty + " already exists. Quit.")
+	sys.exit()
+
+fout = open(outputEmpty, 'w') #write results here
 
 def formatWhitespaces(text):
 	text=text.rstrip() #remove newline
@@ -27,29 +34,25 @@ def formatWhitespaces(text):
 	return text
 
 with open(motifFileWithErrors) as f:
-    for line in f:
-        array=line.rstrip().replace("\t", " ").split(" ") #replace tabs by spaces and split
-        #print array
-        key=str(array[0:3])
-        values=array[3:]
-        errorDict[key]=values
+	for line in f:
+		array=line.rstrip().replace("\t", " ").split(" ") #replace tabs by spaces and split
+		#print array
+		key=str(array[0:3])
+		values=array[3:]
+		errorDict[key]=values
 f.close()
 
-print("printing dictionary")
-for k in errorDict.keys():
-	print k, errorDict[k]
-
-print("parse motifFileWithIPds")
+#print("printing dictionary")
+#for k in errorDict.keys():
+#	print k, errorDict[k]
 
 with open(motifFileWithIPds) as f:
-    for line in f:
-        array=line.rstrip().replace("\t", " ").split(" ") #replace tabs by spaces and split
-        print ("array")
-        print(array)
-        key=array[0:3]
-        values=errorDict[str(key)]
-        merged=key+values
-        merged=reduce(lambda key, values: key+" "+values, merged)
-        print ("dictionary")
-        print(merged)
+	for line in f:
+		array=line.rstrip().replace("\t", " ").split(" ") #replace tabs by spaces and split
+		key=array[0:3]
+		values=errorDict[str(key)]
+		merged=key+values
+		merged=reduce(lambda key, values: key+" "+values, merged)
+		#print(merged)
+		fout.write(merged+"\n")
 
