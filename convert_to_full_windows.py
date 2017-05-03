15aa73cbec74ec4183104d18956898e5a5d18703
biomonika NA <biomonika@psu.edu>
2017-05-03 17:43:52 +0000

    feature coordinates converted to full windows but IPD values completely omitted for now

--- convert_to_full_windows.py
+++ convert_to_full_windows.py
@@ -14,40 +14,46 @@ emptyFile=str(sys.argv[1])
 print emptyFile
 
 outputEmpty=emptyFile.replace("FeatureOnly","FullWindow")
-
 f = open(outputEmpty, 'w') #write results here
 
 #read EmptyTmp file restricted to windows and output full windows
-for line in f:
-	#print i
-	e=line.rstrip() #line from matched empty file
-	e_array=e.split('\t')
-	#print (e_array[0:3])
+with open(emptyFile, "r") as efile:
+	for line in efile:
+		#print(line)
+		e=line.rstrip() #line from matched empty file
+		e_array=e.split('\t')
+		#print (e_array[0:3])
 
-	window_start=int(e_array[1]) #coordinates from empty file
-	window_end=int(e_array[2]) #coordinates from empty file
-	length=int(m_array[3]) #length from motif file
-	tail=e_array[4:len(e_array)]
-		#print ("tail: " + str(tail))
+		window_start=int(e_array[1]) #coordinates from empty file
+		window_end=int(e_array[2]) #coordinates from empty file
+
+		#print("window_start: " + str(window_start) + " window_end: " + str(window_end))
+
+		length=int(e_array[3]) #length from motif file
+		tail=e_array[4:len(e_array)]
+			#print ("tail: " + str(tail))
 
 		#print ("motif length: " + str(length))
+		bp_to_fill=100-length
+		#print ("bp_to_fill: " + str(bp_to_fill))
 
-	if (length % 2 == 0): #even 
-		print "even"
-		window_start = window_start + (50 - math.trunc(length / 2))
-		window_stop = window_start + (50 + math.trunc(length / 2)-1)
-		IPDsubset = tail[(50 - math.trunc(length / 2)):(50 + math.trunc(length / 2))]
-	else: #odd
-		print "odd"
-		window_start = window_start + (50 - math.trunc(length / 2))
-		window_stop = window_start + (50 + math.trunc(length / 2))
-		IPDsubset = tail[(50 - math.trunc(length / 2)):(50 + math.trunc(length / 2) + 1)] #center is 51st nucleotide
+		if (length % 2 == 0): #even 
+			print "even"
+			window_start = window_start - (math.trunc(bp_to_fill / 2))
+			window_end = window_end + (math.trunc(bp_to_fill / 2))
+			#IPDsubset = tail[(50 - math.trunc(length / 2)):(50 + math.trunc(length / 2))]
+		else: #odd
+			print "odd"
+			window_start = window_start - (math.trunc(bp_to_fill / 2)+1)
+			window_end = window_end + (math.trunc(bp_to_fill / 2))
+			#IPDsubset = tail[(50 - math.trunc(length / 2)):(50 + math.trunc(length / 2) + 1)] #center is 51st nucleotide
 
-	print(str(window_stop-window_start+1))
-	res=(e_array[0] + "\t" + str(window_start) + "\t" + str(window_stop) + "\t" + str(length)+ "\t" + '\t'.join(IPDsubset))
-	#print res
-	f.write(res+"\n")
-	#print "***"
+		#print("window_start: " + str(window_start) + " window_end: " + str(window_end))
+		#print(str(window_end-window_start+1))
+		res=(e_array[0] + "\t" + str(window_start) + "\t" + str(window_end) + "\t" + str(length)) #+ "\t" + '\t'.join(IPDsubset))
+		print res
+		f.write(res+"\n")
+		#print "***"
 
 
 
