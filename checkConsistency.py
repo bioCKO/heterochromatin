2ae92965af3d73df110a120d281a761e7c2d802a
biomonika NA <biomonika@psu.edu>
2017-05-03 17:28:36 +0000

    checkConsistency.py edited online with Bitbucket

--- checkConsistency.py
+++ checkConsistency.py
@@ -1,39 +1,53 @@
 import sys
 import os
 from os.path import basename
+import subprocess as sp
+import itertools
+from itertools import izip
+import math
 
 ######################################
-#python checkConsistency.py motifFile
-motifFile=str(sys.argv[1])
+#python convert_to_full_windows.py emptyFile
+emptyFile=str(sys.argv[1])
 ######################################
-formattedFile=motifFile.replace(".txt",".Formatted.txt")
 
-print motifFile
+print emptyFile
 
-f = open(formattedFile, 'w') #write results here
+outputEmpty=emptyFile.replace("FeatureOnly","FullWindow")
+f = open(outputEmpty, 'w') #write results here
 
-#read motifFile and generate an output that is reformatted and checked for the consistency
-i=0
+#read EmptyTmp file restricted to windows and output full windows
+with open(emptyFile, "r") as efile:
+	for line in efile:
+	#print i
+	e=line.rstrip() #line from matched empty file
+	e_array=e.split('\t')
+	#print (e_array[0:3])
 
-with open(motifFile, "r") as ifile:
-	for line in ifile:
-		i=i+1 #line count
-		m=line.rstrip() #line from motif file
-		#print m
-		m_array=m.split(' ')
-		chromosome=m_array[0]
-		try:
-			if (chromosome=="X"):
-				print chromosome
-			else:
-				chromosome=int(chromosome)
-				print chromosome
-			if (i!=1):
-				f.write("\n") #write newline, except for reading first line
-			f.write(m)
-		except ValueError:
-			print "Oops!  That was not valid chromosome.  I will not add newline to this line."
-			last_entry=m_array[-1]
-			print last_entry
-			f.write(m) #the newline shouldn't be there, it's not chromosome
-f.write("\n")
+	window_start=int(e_array[1]) #coordinates from empty file
+	window_end=int(e_array[2]) #coordinates from empty file
+	length=int(m_array[3]) #length from motif file
+	tail=e_array[4:len(e_array)]
+		#print ("tail: " + str(tail))
+
+		#print ("motif length: " + str(length))
+
+	if (length % 2 == 0): #even 
+		print "even"
+		window_start = window_start + (50 - math.trunc(length / 2))
+		window_stop = window_start + (50 + math.trunc(length / 2)-1)
+		IPDsubset = tail[(50 - math.trunc(length / 2)):(50 + math.trunc(length / 2))]
+	else: #odd
+		print "odd"
+		window_start = window_start + (50 - math.trunc(length / 2))
+		window_stop = window_start + (50 + math.trunc(length / 2))
+		IPDsubset = tail[(50 - math.trunc(length / 2)):(50 + math.trunc(length / 2) + 1)] #center is 51st nucleotide
+
+	print(str(window_stop-window_start+1))
+	res=(e_array[0] + "\t" + str(window_start) + "\t" + str(window_stop) + "\t" + str(length)+ "\t" + '\t'.join(IPDsubset))
+	#print res
+	f.write(res+"\n")
+	#print "***"
+
+
+
