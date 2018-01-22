f40955735c5fde9275db1efa85b9e618b0a62f14
biomonika NA <biomonika@psu.edu>
2018-01-22 17:25:00 +0000

    analyze_tandemness.sh created online with Bitbucket

---
+++ analysis/analyze_tandemness.sh
@@ -0,0 +1,43 @@
+grep -v ">" 10_most_abundant.fa | sort | uniq | sort >tmp
+
+species=(Pongo Pan_paniscus Gorilla); 
+
+for sp in "${species[@]}"; do #remove before writing
+	rm ${sp}
+done
+
+while read mf; do #for each repeat motif
+	echo $mf; 
+	for sp in "${species[@]}"; do #for each species
+		echo ${sp}; 
+		echo -n ${mf}" " >>${sp}
+		species_files=`eval echo *${sp}*joinedRepeatFreq.txt`
+		#echo ${species_files}
+		tmpfile=$(mktemp ${sp}.XXXXXX)
+		egrep --color "\b${mf}\b" ${species_files}
+		egrep --color "\b${mf}\b" ${species_files} >${tmpfile}
+		awk '{ sum+=$2} END {printf sum}' ${tmpfile} >>${sp} #joint
+		echo -n " " >>${sp}
+		awk '{ sum+=$3} END {printf sum}' ${tmpfile} >>${sp} #forward
+		echo -n " " >>${sp}
+		awk '{ sum+=$4} END {printf sum}' ${tmpfile} >>${sp} #reverse
+		echo "" >>${sp}
+		rm ${tmpfile}
+	done
+done <tmp
+
+#MERGE INTO SINGLE TABLE
+function multijoin() {
+    out=$1
+    shift 1
+    cat $1 | awk '{print $1}' > $out
+    echo "out" ${out}
+    #for f in $*; do echo "f" ${f}; join $out $f > tmp; mv tmp $out; done
+    for f in $*; do echo "f" ${f}; join -a1 -a2 -o auto -e "NA" $out $f > tmp; mv tmp $out; done
+}
+
+multijoin species.table.txt ${species[@]}
+echo "repeat_motif" ${species[@]} >species.table.txt.with.header.txt
+cat species.table.txt >>species.table.txt.with.header.txt
+
+echo "DONE"
